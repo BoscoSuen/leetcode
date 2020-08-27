c99cb78d455da8a6a25728ed700202a610ab3d4e
Bosco Suen <suen@Boscos-MacBook-Pro.local>
2020-08-26 21:55:38 -0700

    20200826

---
+++ Java/343.integer-break.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=343 lang=java
+ *
+ * [343] Integer Break
+ */
+
+// @lc code=start
+class Solution {
+    public int integerBreak(int n) {
+        
+    }
+}
+// @lc code=end
+
