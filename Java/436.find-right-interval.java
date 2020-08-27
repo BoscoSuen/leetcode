6a0a7f0946112152a315653272be1709e595f70a
Boscosuen <zqsun@eng.ucsd.edu>
2020-08-27 10:59:37 -0700

    20200827

---
+++ Java/436.find-right-interval.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=436 lang=java
+ *
+ * [436] Find Right Interval
+ */
+
+// @lc code=start
+class Solution {
+    public int[] findRightInterval(int[][] intervals) {
+        
+    }
+}
+// @lc code=end
+
