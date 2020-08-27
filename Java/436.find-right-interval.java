ffbe03789eb51937155a8bd93086b067ad8d699b
Boscosuen <zqsun@eng.ucsd.edu>
2020-08-27 11:00:24 -0700

    20200827

--- Java/436.find-right-interval.java
+++ Java/436.find-right-interval.java
@@ -6,8 +6,23 @@
 
 // @lc code=start
 class Solution {
+    /*
+    treemap
+    time: O(nlogn)
+    space: O(n)
+    */
     public int[] findRightInterval(int[][] intervals) {
+        int[] res = new int[intervals.length];
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int i = 0; i < intervals.length; ++i) {
+            map.put(intervals[i][0], i);
+        }
         
+        for (int i = 0; i < intervals.length; ++i) {
+            Integer ceiling = map.ceilingKey(intervals[i][1]);
+            res[i] = ceiling != null ? map.get(ceiling) : -1;
+        }
+        return res;
     }
 }
 // @lc code=end
