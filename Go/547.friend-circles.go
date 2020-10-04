c8c79a9bdf4898c87f848bd234188e02425cc0e6
Boscosuen <zqsun@eng.ucsd.edu>
2020-10-03 17:22:14 -0700

    20201003

---
+++ Go/547.friend-circles.go
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=547 lang=golang
+ *
+ * [547] Friend Circles
+ */
+
+// @lc code=start
+func findCircleNum(M [][]int) int {
+	visited := map[int]bool{}
+	count := 0
+
+	for i := 0; i < len(M); i++ {
+		if !visited[i] {
+			count++
+			dfs(M, visited, i)
+		}
+	}
+	return count
+}
+
+func dfs(M [][]int, visited map[int]bool, pos int) {
+	if visited[pos] {
+		return
+	}
+	visited[pos] = true
+	for next, val := range M[pos] {
+		if val == 1 && !visited[next] {
+			dfs(M, visited, next)
+		}
+	}
+}
+
+// @lc code=end
+
